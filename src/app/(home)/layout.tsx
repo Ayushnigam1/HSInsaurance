ef47118a17edcc889d378c9b898b7dd33a903901
Ayushnigam1 <ashwin200113@gmail.com>
2024-11-08 22:54:55 +0530

    phone and links

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,22 +2,38 @@ import type { Metadata } from "next";
 import "../globals.css";
 import NavBar from "@/components/Navbar";
 import { CssVarsProvider } from "@mui/joy";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import appTheme from "@/theme/theme";
 import Footer from "@/components/Footer";
 import ConvProvider from "@/context/ConvContext/ConvProvider";
 import ContactModal from "@/components/ContactModal";
+import { defineQuery } from "next-sanity";
+import { LandingData } from "@/interface/LandingPage";
+import { client } from "@/sanity/lib/client";
 
 export const metadata: Metadata = {
   title: "Dhanwantri",
   description: "A Insurance Consultancy",
 };
 
-export default function RootLayout({
+const getData = async () => {
+  const LANDING_QUERY = defineQuery(
+    `*[_type=="SiteSettingPage"]{ phone, email, links }[0]`
+  );
+  const landingData: Partial<LandingData> = await client.fetch(
+    LANDING_QUERY,
+    {},
+    { next: { revalidate: 60 } }
+  );
+  return landingData;
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { phone, email, links } = await getData();
   return (
     <html lang="en">
       <body>
@@ -26,8 +42,8 @@ export default function RootLayout({
             <ConvProvider>
               <NavBar />
               {children}
-              <Footer />
-              <ContactModal />
+              <Footer phone={phone} email={email} links={links}  />
+              <ContactModal/>
             </ConvProvider>
           </CssVarsProvider>
         </AppRouterCacheProvider>
